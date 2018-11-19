34dfa3f377272cafcd5bdceae08113c4e4464fcf
zhaorongkui <563374704@qq.com>
2018-11-19 16:17:45 +0800

    首次项目提交
    首次项目提交

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <TestComponent/> -->
+    <Home/>
+  </div>
+</template>
+
+<script>
+// import TestComponent from "./components/test.vue";
+import Home from "./components/Home.vue";
+
+export default {
+  name: "app",
+  components: {
+    // TestComponent,
+    Home
+  }
+};
+</script>
+
+<style>
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(Element, { size: "small", zIndex: 3000 });
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
